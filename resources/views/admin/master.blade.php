ea73db71d17e5453e2d485bc38fef3da26de3a7f
Himanshu Uppal <himanshu94uppal@gmail.com>
2018-07-10 13:06:46 +0530

    changed adsense account

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -10,13 +10,13 @@
 
             gtag('config', 'UA-121702521-1');
         </script>
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "ca-pub-4300142452651464",
-            enable_page_level_ads: true
-            });
-        </script>
+       <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-9716148949716089",
+          enable_page_level_ads: true
+     });
+</script>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
