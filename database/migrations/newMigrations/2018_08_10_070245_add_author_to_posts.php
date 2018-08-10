fefe3ac0bbf349b76d2ff08a316f8b8bd35e99ea
Himanshu Uppal <himanshu94uppal@gmail.com>
2018-08-10 12:37:01 +0530

    added author migration

---
+++ database/migrations/newMigrations/2018_08_10_070245_add_author_to_posts.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAuthorToPosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts',function($table){
+            $table->string('author_name')->after('admin_id');
+            $table->string('author_social_link')->after('author_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts',function($table){
+            $table->dropColumn('author_name');
+            $table->dropColumn('author_social_link');
+        });
+    }
+}
